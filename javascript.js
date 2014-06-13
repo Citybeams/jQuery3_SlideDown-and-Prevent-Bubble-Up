92fc3bd90ae2561bc6942bd2f407618f096b65e1
citybeams <nicklohr1@gmail.com>
2014-06-13 16:46:58 -0400

    prevent bubble up

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// slide down when clicked event
+// prevents bubbleup
+
+
+
+$(document).ready(function() {
+  $(".see-photos").on("click", function(event) {
+    event.stopPropagation();
+    $(this).closest(".tour").find(".photos").slideToggle();
+  });
+  $(".tour").on("click", function() {
+    alert("This should not be called");
+  });
+});
+
